b94c5cb92c82c541f71a7407588f6cebf0bd6c06
tksthii <Nnguyen0011@conestogac.on.ca>
2025-11-09 18:35:42 -0500

    GREEN: Add logic for Paper loses to Scissors - Player2 wins

--- source/repos/RockPaperScissors/rps.c
+++ source/repos/RockPaperScissors/rps.c
@@ -25,6 +25,9 @@ char* rockPaperScissors(const char* player1, const char* player2) {
     if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Paper") == 0) {
         return "Player2";
     }
+    if (strcmp(player1, "Paper") == 0 && strcmp(player2, "Scissors") == 0) {
+        return "Player2";
+    }
 
     return "Draw";
 }
