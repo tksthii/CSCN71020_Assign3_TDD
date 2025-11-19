ae2dd82e8ecfd0140acb067a3bdecb1a68a94ecf
tksthii <Nnguyen0011@conestogac.on.ca>
2025-11-19 15:11:58 -0500

    Setup Native Unit Test Project

---
+++ source/repos/lynn339/TriangleClassificationTests/TriangleClassificationTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleClassificationTests
+{
+	TEST_CLASS(TriangleClassificationTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
